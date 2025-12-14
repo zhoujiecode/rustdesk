d6f7eeaf2bec4ba7c039d553ef51fc1f8e469ec9
zhoujiecode <129595232+zhoujiecode@users.noreply.github.com>
2025-12-14 14:17:03 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rustdesk.zhoujie218.top".to_owned()
 }
 
 #[inline]
